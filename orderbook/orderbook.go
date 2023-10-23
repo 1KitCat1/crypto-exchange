354ef4dbda2d6bb46b381729741290ded83b6da8
Denis Riabtsev <68712122+1KitCat1@users.noreply.github.com>
2023-10-23 11:49:17 +0300

    Add Limit & constructors

--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -1,13 +1,42 @@
 package orderbook
 
+import "time"
+
 type Order struct {
 	ID        int64
 	UserID    int64
 	Size      float64
-	Bid       float64 // limit or market
+	Bid       bool // limit or market
 	Limit     *Limit
 	Timestamp int64
 }
 
-type Limit struct {
+type Orders []*Order
+
+func NewOrder(is_bid bool, size float64) *Order {
+	return &Order{
+		Size:      size,
+		Bid:       is_bid,
+		Timestamp: time.Now().UnixNano(),
+	}
 }
+
+type Limit struct { // group of orders at a certain price limit
+	Price  float64
+	Orders Orders
+	Volume float64
+}
+
+func NewLimit(price float64) *Limit {
+	return &Limit{
+		Price:  price,
+		Orders: []*Order{},
+	}
+}
+
+type Orderbook struct {
+	Asks []*Limit
+	Bids []*Limit
+}
+
+func (o Orders) Len() int { return len(o) }
