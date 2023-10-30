d75cc92f2eedaa9dab1efe9d6284d36499851067
Denis Riabtsev <68712122+1KitCat1@users.noreply.github.com>
2023-10-31 01:25:18 +0300

    Setup basic server

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"crypto-exchange/orderbook"
 	"fmt"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	e := echo.New()
+
+	e.Start(":3000")
+
 	var _ = orderbook.Limit{}
 	fmt.Println("Check")
 }
